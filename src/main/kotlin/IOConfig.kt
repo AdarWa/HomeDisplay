c6eddf9c98468925faa10a69b9a168d1807383b0
AdarWa <adar.wasserman@gmail.com>
2026-03-03 20:56:00 +0200

    Add Dependency injected state provider

---
+++ src/main/kotlin/IOConfig.kt
@@ -0,0 +1,6 @@
+package net.adarw
+
+import net.adarw.hass.states.LocalStateProvider
+import net.adarw.hass.states.StateProvider
+
+val STATE_PROVIDER: StateProvider = LocalStateProvider
---
+++ src/main/kotlin/common/decoders/AnySerializer.kt
@@ -0,0 +1,38 @@
+package net.adarw.common.decoders
+
+import kotlinx.serialization.ContextualSerializer
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.booleanOrNull
+import kotlinx.serialization.json.doubleOrNull
+import kotlinx.serialization.json.longOrNull
+
+object AnySerializer : KSerializer<Any> {
+    @OptIn(ExperimentalSerializationApi::class)
+    override val descriptor: SerialDescriptor =
+        ContextualSerializer(Any::class, null, emptyArray()).descriptor
+
+    override fun serialize(encoder: Encoder, value: Any) {
+    }
+
+    override fun deserialize(decoder: Decoder): Any {
+        val input = decoder as? JsonDecoder ?: throw Exception("Only JSON supported")
+        val element = input.decodeJsonElement()
+        return when (element) {
+            is JsonPrimitive -> {
+                if (element.isString) element.content
+                else element.booleanOrNull ?: element.longOrNull ?: element.doubleOrNull ?: element.content
+            }
+            is JsonArray -> element.map { it.toString() }
+            is JsonObject -> element.toString()
+            else -> element.toString()
+        }
+    }
+}
