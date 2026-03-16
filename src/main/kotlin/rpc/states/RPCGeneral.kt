f4ad995b3236c241613370f340272b42acdc31fe
AdarWa <adar.wasserman@gmail.com>
2026-03-17 01:33:34 +0200

    Add RPC store and registration of devices

--- src/main/kotlin/rpc/states/RPCGeneral.kt
+++ src/main/kotlin/rpc/states/RPCGeneral.kt
@@ -6,11 +6,21 @@ import net.adarw.rpc.RPCEndpoint
 import net.adarw.rpc.definitions.StatusCode
 import net.adarw.rpc.definitions.messages.RegisterMessage
 import net.adarw.rpc.definitions.messages.RegisterResponse
+import net.adarw.store.rpc.RPCStoreManager
 
 private val logger = KotlinLogging.logger {  }
 
 @RPCEndpoint(RegisterMessage::class, RegisterResponse::class, isProtobuf = false)
 fun registerDevice(msg: RegisterMessage): RegisterResponse {
-    logger.info { "Registering Device with serial id ${msg.serialId}" }
-    return RegisterResponse(StatusCode.CREATED.code, HOST, )
+    if(msg.serialId.isEmpty()){
+        logger.error { "serialId cannot be empty while registering new device!" }
+        return RegisterResponse(StatusCode.BAD_REQUEST.code, HOST, -1);
+    }
+    if(RPCStoreManager.serialIdExists(msg.serialId)){
+        logger.error { "serialId ${msg.serialId} already exists in the registry!" }
+        return RegisterResponse(StatusCode.CONFLICT.code, HOST, -1);
+    }
+    val rpcId = RPCStoreManager.registerRPC(msg.serialId)
+    logger.info { "Registering Device with serial id ${msg.serialId}, given id $rpcId" }
+    return RegisterResponse(StatusCode.CREATED.code, HOST, rpcId)
 }
---
+++ src/main/kotlin/store/rpc/RPCStoreManager.kt
@@ -0,0 +1,30 @@
+package net.adarw.store.rpc
+
+import net.adarw.store.DatabaseManager
+import net.adarw.store.property.PropertyStore
+import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.insert
+import org.jetbrains.exposed.v1.jdbc.selectAll
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+
+object RPCStoreManager {
+
+    init {
+        DatabaseManager.instance
+        transaction { SchemaUtils.create(RPCStore) }
+    }
+
+    fun registerRPC(serialId: String): Int =
+         transaction {
+            RPCStore.insert {
+                it[RPCStore.serialId] = serialId
+            } get RPCStore.id
+        }
+
+    fun serialIdExists(serialId: String): Boolean =
+        transaction {
+            RPCStore.selectAll().where { RPCStore.serialId eq serialId }.any()
+        }
+
+}
