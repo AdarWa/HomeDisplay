b40b4f2a1bb0c523fb8132f8f9fd04df2fc34472
AdarWa <adar.wasserman@gmail.com>
2026-03-17 10:31:19 +0200

    Spotless

--- src/main/kotlin/store/rpc/RPCStoreManager.kt
+++ src/main/kotlin/store/rpc/RPCStoreManager.kt
@@ -5,7 +5,6 @@ import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.select
-import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 object RPCStoreManager {
@@ -20,9 +19,9 @@ object RPCStoreManager {
     }
 
     fun serialIdExists(serialId: String): Int? = transaction {
-        RPCStore
-            .select(RPCStore.id)
+        RPCStore.select(RPCStore.id)
             .where { RPCStore.serialId eq serialId }
-            .singleOrNull()?.get(RPCStore.id)
+            .singleOrNull()
+            ?.get(RPCStore.id)
     }
 }
