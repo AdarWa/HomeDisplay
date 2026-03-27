f107025976fac0656c6ae3d5aa45fdd06d73cfdb
AdarWa <adar.wasserman@gmail.com>
2026-03-27 23:34:01 +0300

    Add ConfigParser.kt

---
+++ src/main/kotlin/config/ConfigParser.kt
@@ -0,0 +1,62 @@
+package net.adarw.config
+
+import kotlinx.serialization.Serializable
+import net.adarw.components.DefinedComponent
+import net.adarw.components.NamedComponent
+import net.adarw.components.Screen
+import net.adarw.components.actions.NamedAction
+import net.adarw.components.states.StateDefinition
+import net.mamoe.yamlkt.Yaml
+
+@Serializable
+data class Config(
+    val internalStates: List<StateDefinition>,
+    val actions: List<NamedAction>,
+    val definedComponents: List<DefinedComponent>,
+    val screens: List<Screen>
+)
+
+@Serializable
+data class SimplifiedConfig(
+    val internalStates: List<StateDefinition>,
+    val actions: List<NamedAction>,
+    val screens: List<Screen>
+)
+
+fun parseConfig(config: String) = Yaml.decodeFromString(Config.serializer(), config)
+
+fun parseSimplifiedConfig(config: String): SimplifiedConfig =
+    parseConfig(config).toSimplifiedConfig()
+
+fun encodeSimplifiedConfig(config: SimplifiedConfig) = Yaml.encodeToString(SimplifiedConfig.serializer(), config)
+
+fun Config.toSimplifiedConfig(): SimplifiedConfig {
+    val componentDefinitions =
+        definedComponents
+            .filter { it.name != null }
+            .associateBy { it.name!! }
+
+    val resolvedScreens =
+        screens.map { screen ->
+            val resolvedComponents =
+                screen.components.map { component ->
+                    when (component) {
+                        is NamedComponent ->
+                            componentDefinitions[component.name]
+                                ?: error(
+                                    "No defined component found for name '${component.name}'",
+                                )
+                        is DefinedComponent -> component
+                    }
+                }
+            screen.copy(components = resolvedComponents)
+        }
+
+    return SimplifiedConfig(
+        internalStates = internalStates,
+        actions = actions,
+        screens = resolvedScreens,
+    )
+}
+
+fun encodeConfig(config: Config) = Yaml.encodeToString(Config.serializer(), config)
---
+++ src/test/kotlin/net/adarw/config/ConfigParserTest.kt
@@ -0,0 +1,20 @@
+package net.adarw.config
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ConfigParserTest {
+
+    @Test
+    fun `Test ConfigParser encoding using default config`(){
+        println(encodeConfig(defaultConfig()))
+        assertEquals(encodeConfig(defaultConfig()),encodeConfig(parseConfig(encodeConfig(defaultConfig()))))
+    }
+
+    @Test
+    fun `Test ConfigParser encoding simplified config using default config`(){
+        println("Simplified Config:")
+        println(encodeSimplifiedConfig(defaultConfig().toSimplifiedConfig()))
+    }
+
+}
